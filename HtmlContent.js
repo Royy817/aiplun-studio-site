5d3bcd147017a4d6b0c654602681ea2ed4dc458d
Royy817 <roy.0817.soccer@gmail.com>
2026-06-05 17:35:15 +0900

    Delete HtmlContent.js

--- HtmlContent.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'node:fs';
-import path from 'node:path';
-
-export default function HtmlContent({ file }) {
-  const html = readFileSync(path.join(process.cwd(), 'content', file), 'utf8');
-
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-}
